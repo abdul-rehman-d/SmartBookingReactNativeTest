c9670bdd38d9a869f17e11e0cf2a4ea1e85f620f
Abdul Rehman <ar618211@gmail.com>
2024-08-31 23:57:58 +0500

    feat: animated header done

--- src/components/AnimatedHeader.tsx
+++ src/components/AnimatedHeader.tsx
@@ -1,4 +1,9 @@
-import {View, Dimensions} from 'react-native';
+import {Dimensions, StyleSheet, Pressable} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Animated from 'react-native-reanimated';
+import {useNavigation} from '@react-navigation/native';
+
+import {Icon} from './ui';
 
 type Props = {
   height: number;
@@ -7,13 +12,43 @@ type Props = {
 const screenHeight = Dimensions.get('window').height;
 
 function AnimatedHeader({height}: Props) {
+  const styleHeight = {height: screenHeight * (height / 100)};
+
   return (
-    <View
-      style={{
-        height: screenHeight * (height / 100),
-        backgroundColor: '#00C7BE',
-      }}
-    />
+    <Animated.View
+      style={[styles.header, styleHeight]}
+      sharedTransitionTag="tag">
+      <SafeAreaView style={styles.innerContainer}>
+        <BackButton />
+      </SafeAreaView>
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#00C7BE',
+  },
+  innerContainer: {
+    paddingHorizontal: 20,
+  }
+});
+
+function BackButton() {
+  const navigation = useNavigation();
+
+  function onPress() {
+    navigation.goBack();
+  }
+
+  if (!navigation.canGoBack()) {
+    return null;
+  }
+
+  return (
+    <Pressable onPress={onPress}>
+      <Icon icon="back_arrow" color="#fff" />
+    </Pressable>
   );
 }
 
--- src/screens/Screen2.tsx
+++ src/screens/Screen2.tsx
@@ -1,14 +1,37 @@
-import React from "react";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Text } from "react-native";
-import { AppStackParamList } from "../navigation/AppNavigator";
+import React from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {StyleSheet, Text, View} from 'react-native';
+import {AppStackParamList} from '../navigation/AppNavigator';
+import {AnimatedHeader} from '../components/AnimatedHeader';
 
-type Props = NativeStackScreenProps<AppStackParamList, "Screen1">;
+type Props = NativeStackScreenProps<AppStackParamList, 'Screen2'>;
 
-function Screen2(): React.JSX.Element {
-    return (
-        <Text>Screen2</Text>
-    )
+function Screen2({navigation, route}: Props): React.JSX.Element {
+  const {numberOfLines} = route.params;
+  return (
+    <View style={styles.container}>
+      <AnimatedHeader height={20} />
+      <Text>Screen2 {numberOfLines}</Text>
+    </View>
+  );
 }
 
-export { Screen2 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'space-between',
+  },
+  heading: {
+    color: '#000000',
+    fontSize: 20,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+});
+
+export {Screen2};
